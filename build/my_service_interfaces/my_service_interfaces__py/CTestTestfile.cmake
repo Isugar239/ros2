6289fb7eaff79700900a2d7a6c4de0fe1cb8f1a7
Isugar239 <Za1van@yandex.ru>
2025-10-02 19:08:22 +0300

    delete trash

--- build/my_service_interfaces/my_service_interfaces__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/za1van/ros2_ws/build/my_service_interfaces/my_service_interfaces__py
-# Build directory: /home/za1van/ros2_ws/build/my_service_interfaces/my_service_interfaces__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
